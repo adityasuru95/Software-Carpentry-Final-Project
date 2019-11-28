7f38beb45cabdfc5303c29e6748ebbfb8a0290d4
sherlinci <54916018+sherlinci@users.noreply.github.com>
2019-11-28 00:07:05 -0500

    Update 1_fundamentals.dart

--- Dart Learning/1_fundamentals.dart
+++ Dart Learning/1_fundamentals.dart
@@ -1,6 +1,30 @@
-import 'dart:io';
+import 'dart:io' ;
 
 void main() {
-  stdout.writeln("Hello world") ;
-  print("helloworld") ;
+  var firstName = 'Aditya' ; 
+  String lastName = "Suru" ;
+  int age ;
+  double money ;
+
+  age = 24 ; 
+  money = 200.200 ;
+  
+  print("my name is $firstName." + ' ' + lastName + ' My age is $age.' + " I have $money money") ;
+
+  print("Your name is: ") ;
+  var yourName = stdin.readLineSync() ;
+  stdout.writeln("Your name is $yourName") ;
+
+  // This is an inline comment
+
+  /// this is a
+  /// documentation comment
+  
+  /* this is a
+  multiple line
+  comment
+  */
+
+  
+
 }
